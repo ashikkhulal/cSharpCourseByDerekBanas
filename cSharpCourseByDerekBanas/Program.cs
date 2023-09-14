2a308b295ed3e5f3a1b4dd46b97a1ae6297d8e84
Ashik Khulal <askhulal@gmail.com>
2023-09-14 12:33:17 -0500

    Initialized a git repo and added first lessons

---
+++ cSharpCourseByDerekBanas/Program.cs
@@ -0,0 +1,84 @@
+﻿using System;
+
+namespace cSharpCourseByDerekBanas
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            //Console.WriteLine("Before");
+            //Console.ForegroundColor = ConsoleColor.Red;
+            //Console.WriteLine("Intermediate");
+            //Console.BackgroundColor = ConsoleColor.White;
+            //Console.WriteLine("After");
+
+            //Console.Write("What is your name? ");
+            //string name = Console.ReadLine();
+            //Console.WriteLine($"Your name is {name}");
+
+            /*
+            bool canITalk = true;
+            Console.WriteLine("Biggest integer: {0}", int.MaxValue);
+            Console.WriteLine("Smallest integer: {0}", int.MinValue);
+
+            Console.WriteLine("Biggest long: {0}", long.MaxValue);
+            Console.WriteLine("Smallest long: {0}", long.MinValue);
+
+            Console.WriteLine("Biggest decimal: {0}", decimal.MaxValue);
+            Console.WriteLine("Smallest decimal: {0}", decimal.MinValue);
+
+            Console.WriteLine("Biggest double: {0}", double.MaxValue);
+            Console.WriteLine("Smallest double: {0}", double.MinValue);
+
+            Console.WriteLine("Biggest float: {0}", float.MaxValue);
+            Console.WriteLine("Smallest float: {0}", float.MinValue);
+            */
+
+            // Explicit conversion (may lose data)
+
+            //bool boolFromStr = bool.Parse("true");
+            //int intFromStr = int.Parse("100");
+            //double dblFromStr = double.Parse("1.234");
+
+            //string myString = 100.ToString();
+            //Console.WriteLine($"Data type: {myString.GetType()}");
+
+            //double dblNum = 10.3567;
+            //Console.WriteLine($"Integer: {(int)dblNum}");
+
+
+            //// Implicit conversion
+
+            //int intNum = 10;
+            //long longNum = intNum;
+            //decimal decimalNum = intNum;
+            //Console.WriteLine($"longNum: {longNum}");
+            //Console.WriteLine($"decimalNum: {decimalNum}");
+
+            //Console.WriteLine("Currency: {0:c}", 20.206);
+            //Console.WriteLine("Pad with 0s: {0:d5}", 20);
+            //Console.WriteLine("4 decimals: {0:f4}", 20.2067282);
+            //Console.WriteLine("Commas: {0:n0}", 202067282);
+
+            //string randString = "My random string";
+            //Console.WriteLine($"String length is: {randString.Length}");
+            //Console.WriteLine("String Contains 'random': {0}", randString.Contains("random"));
+            //Console.WriteLine("Index of 'random' within string: {0}", randString.IndexOf("random"));
+            //Console.WriteLine("Removing 'random' from string: {0}", randString.Remove(3, 7));
+            //Console.WriteLine("Inserting 'new' into string: {0}", randString.Insert(3, "new "));
+            //Console.WriteLine("Replacing 'random' with 'new random' in string: {0}", randString.Replace("random", "new random"));
+            //Console.WriteLine("Compare Two to Three: {0}", String.Compare("Two", "Three", StringComparison.OrdinalIgnoreCase));
+
+            // Start from 36:00
+
+
+
+
+
+
+
+
+
+        }
+    }
+}
